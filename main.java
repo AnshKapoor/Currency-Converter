18acb37b2fb4756b245a2d4381949075af7a92c7
Ansh Kapoor <ansh605@gmail.com>
2018-10-10 20:56:02 +0530

    Added base code for for the main class

---
+++ main.java
@@ -0,0 +1,9 @@
+import java.awt.*;        // Using AWT container and component classes
+import java.awt.event.*;  // Using AWT event classes and listener interfaces
+
+public class AWTCounter extends Frame implements ActionListener{
+
+
+
+
+}
