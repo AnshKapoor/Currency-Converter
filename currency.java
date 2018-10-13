59d126124083f84852f5364bdd568708cef982e5
Ansh Kapoor <32305554+AnshKapoor@users.noreply.github.com>
2018-10-13 20:21:13 +0530

    Added Dropdown lists and other options
    Three things have been added. 1.Text field and source dropdown list, 2. Destination Dropdown list, 3. Convert Button

--- currency.java
+++ currency.java
@@ -38,7 +38,43 @@ public class currency extends JFrame {
 			}
 		});
 	}
+public currency() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		String[] currencies = {"USD","INR","EUR","CAD"};
+		JComboBox comboBox = new JComboBox(currencies);
+		comboBox.setBackground(UIManager.getColor("CheckBox.background"));
+		comboBox.setBounds(31, 33, 73, 22);
+		contentPane.add(comboBox);
+		SpinnerModel model = new SpinnerNumberModel(0, 1, 15, 0.1);     
+		JSpinner spinner = new JSpinner(model);
+		spinner.setBounds(114, 34, 86, 20);
+		contentPane.add(spinner);
+		
+		JComboBox comboBox_1 = new JComboBox(currencies);
+		comboBox_1.setBounds(276, 33, 73, 22);
+		contentPane.add(comboBox_1);
+		
+		JLabel lblTo = new JLabel("TO");
+		lblTo.setBounds(233, 37, 23, 14);
+		contentPane.add(lblTo);
+		
+		JButton btnConvert = new JButton("Convert");
+		btnConvert.setFont(new Font("Simplified Arabic Fixed", Font.PLAIN, 15));
+		btnConvert.setBounds(157, 94, 99, 32);
+		contentPane.add(btnConvert);
+		
+	}
+}
 
+	
+	
+	
+	
 	/**
 	 * Create the frame.
 	 */
