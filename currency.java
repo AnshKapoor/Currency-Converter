951f0b7cd300b062fea527766041722e56107490
Ansh Kapoor <32305554+AnshKapoor@users.noreply.github.com>
2018-10-28 23:39:42 +0530

    Added static function to identify currency from index value

--- currency.java
+++ currency.java
@@ -10,7 +10,29 @@ public class currency {
 		return result; 
 		
 	}
+	public static currency Identify(int index) {
+		currency c;
+		if(index==0)
+		{   
+			c=new currency("Rupees",73.67);
+		}
+		else if(index==1) {
+			c= new currency("CAD",1.31);
+		}
+		else if(index==2) {
+			c= new currency("AUS",1.41);
+		}
+		else if(index==3) {
+			c= new currency("Euro",0.87);
+		}
+		else {
+			c= new currency("Kuwaiti Dinar",0.30);
+		}
+		return c;
+		
+	}
 	
+
 public currency(String name,double rate) {
 	this.name=name;
 	this.rate = rate;
